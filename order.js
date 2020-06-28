f95c5a6e8cba4bc7af925c8ba47cb76844b5f37d
Jiaying <cooolnichole@gmail.com>
2020-06-29 05:53:58 +0800

    All done without limiting customer access

--- order.js
+++ order.js
@@ -34,10 +34,12 @@ document.querySelector('#logout').addEventListener('click', (e) => {
 
 readNewOrder();
 readAcceptedOrder();
+readCompletedOrder();
+
 
 function readNewOrder() {
     firebase.firestore().collection("Order").onSnapshot(function(snapshot) {
-        document.getElementById("NewOrderSection").innerHTML="<h4>New Orders</h4></br>";
+        document.getElementById("NewOrderSection").innerHTML="<h4>New Orders| Accept Now</h4></br>";
         snapshot.forEach(function(taskValue){
           if(taskValue.data().foodStoreId == firebase.auth().currentUser.email && taskValue.data().status == 1){
             document.getElementById("NewOrderSection").innerHTML+= 
@@ -51,7 +53,7 @@ function readNewOrder() {
                 <p class="NewOrder-quantity">Quantity: ${taskValue.data().dishes[i].quantity}</p>`
             }
             document.getElementById("NewOrderSection").innerHTML+= 
-            `<p class="NewOrder-price">Price: ${taskValue.data().totalPrice}</p>
+            `<p class="NewOrder-price">Total Price: ${taskValue.data().totalPrice}</p>
            
             <button type="submit" style="color:white" class="btn btn-warning" 
                       onclick="accept('${taskValue.id}')">Accept Order</button>
@@ -84,27 +86,25 @@ function reject(id) {
 
 function readAcceptedOrder() {
     firebase.firestore().collection("Order").onSnapshot(function(snapshot) {
-        document.getElementById("AcceptedOrderSection").innerHTML="<h4>New Orders</h4></br>";
+        document.getElementById("CompleteOrderSection").innerHTML="<h4>Accepted Orders | Cook Now</h4></br>";
         snapshot.forEach(function(taskValue){
-          if(taskValue.data().foodStoreId == firebase.auth().currentUser.email && taskValue.data().status == 1){
-            document.getElementById("AcceptedOrderSection").innerHTML+= 
-              `<div class="NewOrder mb-3">
-                  <div class="NewOrder-body">
-                      <p class="NewOrder-customer">Customer id: ${taskValue.data().userId}</h5>
-                      <p class="NewOrder-orderid">Order id: ${taskValue.id}</h5>`;
+          if(taskValue.data().foodStoreId == firebase.auth().currentUser.email && taskValue.data().status == 2){
+            document.getElementById("CompleteOrderSection").innerHTML+= 
+              `<div class="CompleteOrder mb-3">
+                  <div class="CompleteOrder-body">
+                      <p class="CompleteOrder-customer">Customer id: ${taskValue.data().userId}</h5>
+                      <p class="CompleteOrder-orderid">Order id: ${taskValue.id}</h5>`;
             for (i = 0; i < taskValue.data().dishes.length; i++) {
-                document.getElementById("NewOrderSection").innerHTML+= `
-                <p class="NewOrder-dish">Dish: ${taskValue.data().dishes[i].name}</p>
-                <p class="NewOrder-quantity">Quantity: ${taskValue.data().dishes[i].quantity}</p>`
+                document.getElementById("CompleteOrderSection").innerHTML+= `
+                <p class="CompleteOrder-dish">Dish: ${taskValue.data().dishes[i].name}</p>
+                <p class="CompleteOrder-quantity">Quantity: ${taskValue.data().dishes[i].quantity}</p>`
             }
-            document.getElementById("NewOrderSection").innerHTML+= 
-            `<p class="NewOrder-price">Price: ${taskValue.data().totalPrice}</p>
+            document.getElementById("CompleteOrderSection").innerHTML+= 
+            `<p class="CompleteOrder-price">Total Price: ${taskValue.data().totalPrice}</p>
            
             <button type="submit" style="color:white" class="btn btn-warning" 
-                      onclick="accept('${taskValue.id}')">Accept Order</button>
-            <button type="submit" class="btn btn-danger" onclick="reject('${taskValue.id}')">
-            Reject Order
-            </button>
+                      onclick="complete('${taskValue.id}')">Order Completed</button>
+            
             <p>-----------------------------------------------</p>
             </div>
                   </div>`    
@@ -113,3 +113,47 @@ function readAcceptedOrder() {
         });
     });
   }
+
+  function complete(id) {
+    let db = firebase.firestore().collection("Order").doc(id);
+    db.update({
+    "status": 4,
+  });
+}
+
+function readCompletedOrder() {
+    firebase.firestore().collection("Order").onSnapshot(function(snapshot) {
+        document.getElementById("AcceptedOrderSection").innerHTML="<h4>Wait for Collection & Payment</h4></br>";
+        snapshot.forEach(function(taskValue){
+          if(taskValue.data().foodStoreId == firebase.auth().currentUser.email && taskValue.data().status == 4){
+            document.getElementById("AcceptedOrderSection").innerHTML+= 
+              `<div class="AcceptedOrder mb-3">
+                  <div class="AcceptedOrder-body">
+                      <p class="AcceptedOrder-customer">Customer id: ${taskValue.data().userId}</h5>
+                      <p class="AcceptedOrder-orderid">Order id: ${taskValue.id}</h5>`;
+            for (i = 0; i < taskValue.data().dishes.length; i++) {
+                document.getElementById("AcceptedOrderSection").innerHTML+= `
+                <p class="AcceptedOrder-dish">Dish: ${taskValue.data().dishes[i].name}</p>
+                <p class="AcceptedOrder-quantity">Quantity: ${taskValue.data().dishes[i].quantity}</p>`
+            }
+            document.getElementById("AcceptedOrderSection").innerHTML+= 
+            `<p class="AcceptedOrder-price">Total Price: ${taskValue.data().totalPrice}</p>
+           
+            <button type="submit" style="color:white" class="btn btn-warning" 
+                      onclick="paid('${taskValue.id}')">Order Completed</button>
+            
+            <p>-----------------------------------------------</p>
+            </div>
+                  </div>`    
+                  
+          }
+        });
+    });
+  }
+
+  function paid(id) {
+    let db = firebase.firestore().collection("Order").doc(id);
+    db.update({
+    "status": 5,
+  });
+}
